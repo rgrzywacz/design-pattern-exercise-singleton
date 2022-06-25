26a7663d287518eb8bf1aa82665fe551c7efbc45
Rafał Grzywacz <rafal.grzywacz@comarch.pl>
2022-06-25 21:55:54 +0200

    first commit

---
+++ src/main/java/account/TransferService.java
@@ -0,0 +1,11 @@
+package account;
+
+public class TransferService {
+    public void transfer(final String accountNumberSource,final String accountNumberDestination, double ammount) {
+        if(ammount>0 && AccountNumberValidator.validate(accountNumberSource)
+           && AccountNumberValidator.validate(accountNumberDestination)) {
+            System.out.println("money from " + accountNumberSource + " to " + accountNumberDestination + " has been transferred ...");
+
+        }
+    }
+}
